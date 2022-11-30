47ad2f36d928c95469b98fbb1829d7de765f8f7b
jtkaufman737 <jtkaufman737@gmail.com>
2022-11-30 15:27:09 -0500

    test

--- pages/api/locations.js
+++ pages/api/locations.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 export default async function handler(req, res) {
   try {
     const { longitude, latitude } = req.query 
-    console.log("iN LOCATIONS ENDPOINT", req)
+    console.log("iN LOCATIONS ENDPOINT", req, latitude, longitude)
     const baseUrl = process.env.NEXT_PUBLIC_API_TARGET
     const url = `${baseUrl}${latitude},${longitude}&per_page=10`
     const locations = await axios.get(url).then(res => {
       console.log(res.data)
       return res.data
-    })
+    }).catch(err => console.log("GET BREWERY ERROR", err))
     return res.status(200).json(locations)
   } catch(e) {
     console.error("YO I ERRORED IN THE LOCATION ENDPOINT", e)
