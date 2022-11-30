62a72fa0d4cda7e4e4b3b6217669abe66f9b7501
jtkaufman737 <jtkaufman737@gmail.com>
2022-11-30 13:17:54 -0500

    test

--- middleware.js
+++ middleware.js
@@ -2,15 +2,17 @@
 import { NextRequest } from "next/server"
 import { MiddlewareRequest } from '@netlify/next';
 
-export async function middleware(req = NextRequest) {
-
+export async function middleware(req) {
+  console.log("REQ", req)
   // we'll use NY, NY as a default in lieu of good data 
   const middlewareRequest = new MiddlewareRequest(req)
+  console.log(middlewareRequest)
   const longitude = req.geo.longitude ? req.geo.longitude : "-74.0060" 
   const latitude = req.geo.latitude ? req.geo.latitude : "40.7128"     
   const response = await middlewareRequest.next();
   console.log("MIDDLEWARE RESPONSE", response)
   response.setPageProp("latitude", latitude)
   response.setPageProp("longitude", longitude)
+  console.log("RESPONSE AFTER SET PAGE PROP", response)
   return response;
 }
