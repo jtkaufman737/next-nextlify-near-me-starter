72d8e8c3807e6b638353c1aa0c812eb34d2d94ef
jtkaufman737 <jtkaufman737@gmail.com>
2022-11-30 13:51:19 -0500

    test just skipping long/lat, try to get more info

--- middleware.js
+++ middleware.js
@@ -10,9 +10,8 @@ export async function middleware(req) {
   const longitude = req.geo.longitude ? req.geo.longitude : "-74.0060" 
   const latitude = req.geo.latitude ? req.geo.latitude : "40.7128"     
   const response = await middlewareRequest.next();
-  console.log("MIDDLEWARE RESPONSE", response)
-  response.setPageProp("latitude", latitude)
-  response.setPageProp("longitude", longitude)
+  // console.log("MIDDLEWARE RESPONSE", response)
+
   console.log("RESPONSE AFTER SET PAGE PROP", response)
   return response;
 }
