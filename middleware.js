96a941326436fa435607ce46a7be6c9f98cec2a7
jtkaufman737 <jtkaufman737@gmail.com>
2022-11-30 11:51:18 -0500

    test new config details

--- middleware.js
+++ middleware.js
@@ -1,13 +1,14 @@
 
-import { NextResponse } from 'next/server'
+import { MiddlewareRequest } from '@netlify/next';
 
 export async function middleware(req) {
-  const { nextUrl: url, geo } = req
+  const pathname = req.nextUrl.pathname;
+
   // we'll use NY, NY as a default in lieu of good data 
-  const longitude = geo.longitude ? geo.longitude : "-74.0060" 
-  const latitude = geo.lattitude ? geo.latitude : "40.7128" 
-  url.searchParams.set('longitude', longitude)
-  url.searchParams.set('latitude', latitude)
-  
-  return NextResponse.rewrite(url)
+  const longitude = req.geo.longitude ? req.geo.longitude : "-74.0060" 
+  const latitude = req.geo.lattitude ? req.geo.latitude : "40.7128" 
+  const middlewareRequest = new MiddlewareRequest(req)
+  const response = await middlewareRequest.next();
+  console.log(response)
+  return response;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import getConfig from 'next/config'
 
 const axios = require('axios');
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-
 
 export async function getServerSideProps({ query }) {
   // This links certain middleware properties we need for geolocation
@@ -22,14 +18,24 @@ export async function getServerSideProps({ query }) {
 export default function Home({ query }) {
   // we will use NY, NY in lieu of location data from middleware
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/locations`
+  /* 
+  Named fields are the JSON properties to build out as columns in our table of results
+  Place type is the label throughout the app, for me "Brewery"
+  Logo is our imagery, for me a beer mug I found on iconfinder.com
+  */
   const namedFields = process.env.NEXT_PUBLIC_NAMED_FIELDS.split(",")
   const placeType = process.env.NEXT_PUBLIC_PLACE_TYPE
   const logo = process.env.NEXT_PUBLIC_ICON_IMG
   const [locations, setLocations] = useState([]);
 
+  /* 
+  We will use the useEffect hook to kick off our API call process 
+  on load, and we won't provide it a dependency array for now because
+  we aren't very concerned with re-fetching
+  */
   useEffect(() => {
     (async() => {
-      const response = await axios.get(
+      await axios.get(
         url,
         { 
           params: { 
@@ -48,13 +54,13 @@ export default function Home({ query }) {
         <meta name="description" content={`${placeType} finder application`} />
         <link rel="icon" href={logo} />
       </Head>
-
+      {/* page title */}
       <main className={styles.main}>
         <h1 className={styles.title}>
           Brewery finder 
         </h1>
         <div className="imageWrapper">
-          <Image id="headerImage" width="60px" height="60px" alt="beer" src="/beer.png"/>
+          <Image id="headerImage" width="60" height="60" alt="beer" src={logo}/>
         </div>
         <div className="resultList">
           { locations && (
